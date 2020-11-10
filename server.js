0e4105eab9025c979d0c02b436b8057704b48f3b
Jacob Madsen <jmadsen24@Jacobs-MBP.lan>
2020-11-10 14:41:28 -0600

    cookie route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
 
 
+app.use(cookieParser())
 app.listen(8080, () => console.log("heatlh check is live!"));
 
 app.get('/healthFail', (req, res, next) => {
@@ -20,4 +22,9 @@ app.get('/healthSuccess', (req, res, next) => {
     status: req.url === '/healthFail' ? 500 : 200
   })
   res.sendStatus(200);
+})
+
+app.get('/cookie', (req, res) => {
+  console.log(req.cookies);
+  res.send(req.cookies);
 })
